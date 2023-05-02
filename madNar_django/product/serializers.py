1556984992e5815388ff88fd12731526a853ce3d
MakanKn <makankananian@gmail.com>
2023-05-02 17:19:34 +0330

    First commit

---
+++ madNar_django/product/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Category, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ("id",
+                  "name",
+                  "get_absoulute_url",
+                  "description",
+                  "price",
+                  "get_image",
+                  "get_thumbnail"
+                  )
+        
