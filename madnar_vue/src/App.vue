b7e6eada8e570c3ba63ea6dc19d9a8802c859950
MakanKn <makankananian@gmail.com>
2023-05-04 02:21:25 +0330

    nav bar changed, no logo, footer fixed

--- madnar_vue/src/App.vue
+++ madnar_vue/src/App.vue
@@ -2,55 +2,46 @@
 
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
-      <div class="icon_start">
-        <i class="mdi mdi-36px"></i>
-      </div>
-      <div class="navbar-brand">
-        
-          <!-- <i class="fa-solid fa-bag-shopping" style="color: #354b7e;"></i> -->
-        
-        <router-link to="/" class="navbar-item"><strong>MadNar</strong></router-link>
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="logo navbar-item" href="/">
+        <img src="https://img.icons8.com/ios-filled/50/null/dress-back-view.png"/>
+      </a>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      
-      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
-        <div class="navbar-end">
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Categories
-            </a>
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" @click="showMobileMenu = !showMobileMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-            <div class="navbar-dropdown">
-              <router-link to="/summer" class="navbar-item">
-                Summer
-              </router-link>
-              <router-link to="/winter" class="navbar-item">
-                Winter
-              </router-link>
-            </div>
-          </div>
+    <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileMenu }">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item" exact :class="{ 'router-link-exact-active': $route.path === '/' }"><strong>MadNar</strong></router-link>
 
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Categories</a>
 
-          <div class="navbar-item">
-            <div class="buttons">
-              <router-link to="/log-in" class="button is-light">Log in</router-link>
-
-              <router-link to="/cart" class="button is-success">
-                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart ({{ cartTotalLength}})</span>
-              </router-link>
-
-            </div>
+          <div class="navbar-dropdown">
+            <router-link to="/summer" class="navbar-item">Summer</router-link>
+            <router-link to="/winter" class="navbar-item">Winter</router-link>
           </div>
         </div>
       </div>
-    </nav>
+
+      <div class="navbar-end">
+        <router-link to="/cart" class="navbar-item">
+          <span class="icon has-badge-rounded has-badge-success" :data-count="cartTotalLength"><i class="fas fa-shopping-cart"></i></span>
+        </router-link>
+
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/log-in" class="button is-light">Log in</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 
     <section class="section">
       <router-view/>
@@ -85,8 +76,12 @@
         </div>
       </div>
     </footer>
+
+    
   </div>
+  
 </template>
+
 
 <script>
 export default {
@@ -118,36 +113,85 @@ export default {
 }
 </script>
 
+
+
 <style scoped>
+
+.navbar {
+  background-color: #000000;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+}
+
+.navbar-item:hover {
+  background-color: transparent !important;
+}
+
+.navbar-item.has-dropdown.is-active > a {
+  background-color: transparent !important;
+}
+
+.navbar-link {
+  color: #ffffff;
+}
+
+.navbar-dropdown {
+  background-color: #ffffff;
+}
+
+.navbar-dropdown a {
+  color: #363636;
+}
+
+.navbar-dropdown a:hover {
+  background-color: #f5f5f5;
+}
+
+.navbar-burger {
+  color: #ffffff;
+}
+
+.icon.has-badge-rounded {
+  position: relative;
+}
+
+.icon.has-badge-rounded::after {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  content: attr(data-count);
+  font-size: 10px;
+  color: #ffffff;
+  background-color: #4caf50;
+  border-radius: 50%;
+  padding: 2px 5px;
+}
+
 .footer {
   background-color: #2f2f2f;
   color: #fff;
   padding: 50px 0;
 }
-
 .footer h2 {
   color: #fff;
 }
-
 .social-icons {
   display: flex;
   justify-content: center;
 }
-
 .social-icons a {
   color: #fff;
   font-size: 24px;
   margin-right: 20px;
 }
-
 .social-icons a:last-child {
   margin-right: 0;
 }
+
+
 </style>
 
 <style lang="scss">
 @import '../node_modules/bulma';
-@import '//node_modules/madnar_vue/node_modules/bulma-social/social-providers';
 </style>
 
 
